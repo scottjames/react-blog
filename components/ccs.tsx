a9ad64fa7c3a4ab5313e59d7c7d75dd8310426f1
Scott <saj@dill.lan>
2024-08-10 18:29:02 -0400

    add ccs component

---
+++ components/ccs.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+import TestimonialImage01 from "@/public/images/testimonial-01.jpg";
+import TestimonialImage02 from "@/public/images/testimonial-02.jpg";
+import TestimonialImage03 from "@/public/images/testimonial-03.jpg";
+
+export default function Computercsi() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+          {/* CCS Header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 mb-4">computercsi.com</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
